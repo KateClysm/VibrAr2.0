000538a0c845843aeccd2b99ca4112c03b7fcf4c
Katerina <kate.clysm@gmail.com>
2024-02-02 17:01:31 -0300

    Boceto de estructura

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,54 +1,94 @@
 import "./homepage.scss";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { CiCircleChevLeft } from "react-icons/ci";
 import { CiCircleChevRight } from "react-icons/ci";
+
+import { motion } from "framer-motion";
+
 function HomePage() {
+  const [artistsList, setArtistsList] = useState([]);
+  const [index, setIndex] = useState(0);
 
-    const [artistsList, setArtistsList] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('/src/assets/artists.json');
-                const data = await response.json();
-
-                const artistsArray = Object.values(data);
-                setArtistsList(artistsArray);
-            } catch (error) {
-                console.error('Error fetching artists data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-
-
-    const [index, setIndex] = useState(0);
-
-    function nextStep() {
-        if (index === artistsList.length - 1){
-            setIndex(0);
-            return
+  const variants = {
+    initial: (direction) => {
+      return {
+        x: direction > 0 ? 800 : -800,
+        opacity: 0,
+        scale: 0.8,
+      };
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      scale: 1,
+      transition: {
+        x: {type: 'spring', stiffness: 300, damping: 30 },
+        opacity: { duration: 0.3 }
+      }
+    },
+    exit: (direction) => {
+      return {
+        x: direction > 0 ? -800 : 800,
+        opacity: 0,
+        scale: 0.8,
+        transition: {
+          x: {type: 'spring', stiffness: 300, damping: 30 },
+          opacity: { duration: 0.3 }
         }
-        setIndex(index + 1)
+      };
+    },
+  };
+
+  const [direction, setDirection] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/src/assets/artists.json");
+        const data = await response.json();
+
+        const artistsArray = Object.values(data);
+        setArtistsList(artistsArray);
+      } catch (error) {
+        console.error("Error fetching artists data:", error);
+      }
     };
-    function prevStep() {
-        if (index === 0){
-            setIndex(artistsList.length - 1);
-            return
-        }
-        setIndex(index - 1)
-    };
+
+    fetchData();
+  }, []);
+
+  function nextStep() {
+    setDirection(1);
+    if (index === artistsList.length - 1) {
+      setIndex(0);
+      return;
+    }
+    setIndex(index + 1);
+  }
+
+  function prevStep() {
+    setDirection(-1);
+    if (index === 0) {
+      setIndex(artistsList.length - 1);
+      return;
+    }
+    setIndex(index - 1);
+  }
 
   return (
     <section className="homepage">
-      <div className="container">
+      <div className="container-slider">
         <div className="slide-show">
           {artistsList.length > 0 && artistsList[index] && (
-            <img
+            <motion.img
+              variants={variants}
+              animate="animate"
+              initial="initial"
+              exit="exit"
+              key={artistsList[index]?.banner || "defaultKey"}
+              custom={direction}
               src={`/baners_artistas/${artistsList[index].banner}`}
-              alt=""
+              alt={artistsList[index].nombre}
               className="slide"
             />
           )}
@@ -61,82 +101,20 @@ function HomePage() {
           </button>
         </div>
       </div>
+
+      <div className="container-info">
+            <div className="oficiales"></div>
+            <div className="gossip"></div>
+      </div>
+
+      <div className="container-fotografias">
+        <h3>Nuestro granito de arena</h3>
+        <div className="grid-fotos">
+
+        </div>
+      </div>
     </section>
   );
 }
 
 export default HomePage;
-
-
-
-
-// import "./homepage.scss";
-// import React, { useState, useEffect } from 'react';
-// import { CiCircleChevLeft } from "react-icons/ci";
-// import { CiCircleChevRight } from "react-icons/ci";
-
-// function HomePage() {
-
-//     const [artistsList, setArtistsList] = useState([]);
-
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             try {
-//                 const response = await fetch('/src/assets/artists.json');
-//                 const data = await response.json();
-
-//                 const artistsArray = Object.values(data);
-//                 setArtistsList(artistsArray);
-//             } catch (error) {
-//                 console.error('Error fetching artists data:', error);
-//             }
-//         };
-
-//         fetchData();
-//     }, []);
-
-
-
-//     const [index, setIndex] = useState(0);
-
-//     function nextStep() {
-//         if (index === artistsList.length - 1){
-//             setIndex(0);
-//             return
-//         }
-//         setIndex(index + 1)
-//     };
-//     function prevStep() {
-//         if (index === 0){
-//             setIndex(artistsList.length - 1);
-//             return
-//         }
-//         setIndex(index - 1)
-//     };
-
-//   return (
-//     <section className="homepage">
-//       <div className="container">
-//         <div className="slide-show">
-
-//           {artistsList.lenght > 0 &&(
-//             <img
-//             src={`/baners_artistas/${artistsList[index].banner}`}
-//             alt=""
-//             className="slide"
-//           />
-//           )}
-
-//           <button className="prevButton" onClick={prevStep}>
-//           <CiCircleChevLeft />
-//           </button>
-//           <button className="nextButton" onClick={nextStep}>
-//           <CiCircleChevRight />
-//           </button>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default HomePage;
